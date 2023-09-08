3a86bae29c0bc5498071b936605650b7902d18fb
Etienne Maway <etiennemaway@gmail.com>
2023-09-09 01:59:06 +0300

    FlashLoan Request functionality

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -17,6 +17,15 @@ contract FlashLoan {
 
   address private constant deployer =0x41ff9AA7e16B8B1a8a8dc4f0eFacd93D02d071c9;
 
+  struct FlashCallbackData {
+    uint amount0;
+    uint amount1;
+    address caller;
+    address[2] path; // coins to be swapped
+    uint[3] exchRoute; // swapping three times before paying the loan
+    uint24 fee;
+  }
+
   constructor(address _token0, address _token1, uint24 _fee){
     token0 = IERC20(_token0);
     token1 = IERC20(_token1);
@@ -24,10 +33,50 @@ contract FlashLoan {
     console.log("The pool address: \t", address(pool));
   }
 
+  function flashLoanRequest(
+    address[2] memory _path,
+    uint _amount0,
+    uint _amount1,
+    uint24 _fee,
+    uint[3] memory _exchRoute
+  ) external {
+    // Encoding data for the machine to understand and process
+    bytes memory data = abi.encode(FlashCallbackData({
+      amount0: _amount0,
+      amount1: _amount1,
+      caller : msg.sender,
+      path: _path,
+      exchRoute:_exchRoute,
+      fee: _fee
+    }));
+    console.log("");
+    console.log("FlashLoand pool address: ", address(pool));
+    IUniswapV3Pool(pool).flash(address(this), _amount0, _amount1, data);
+  }
+
+   function pancakeV3FlashCallback(
+    uint fee0,
+    uint fee1,
+    bytes calldata data
+  ) external {
+    require(msg.sender == address(pool), "Unauthorized!");
+
+    FlashCallbackData memory decoded = abi.decode(data, (FlashCallbackData));
+
+    // Initialize
+    IERC20 baseToken = (fee0 > 0) ? token0 : token1;
+    uint acquiredAmount = (fee0 > 0) ? decoded.amount0 : decoded.amount1;
+    console.log("feeO ", fee0);
+    console.log("fee1 ", fee1);
+    console.log("BaseToken ", address(baseToken));
+    console.log("Acquired Amount", acquiredAmount);
+  }
+
   function getPool(address _token0, address _token1, uint24 _fee) public pure returns(address ){
     PoolAddress.PoolKey memory poolKey = PoolAddress.getPoolKey(_token0, _token1, _fee);
     return PoolAddress.computeAddress(deployer, poolKey);
   }
+
 }
 
 library PoolAddress {
@@ -40,12 +89,15 @@ library PoolAddress {
     uint24 fee;
   }
 
+// Getting the pool key
   function getPoolKey(
     address tokenA,
     address tokenB,
     uint24 fee
   ) internal pure returns(PoolKey memory){
-    if(tokenA > tokenB) (tokenA, tokenB) = (tokenB, tokenA);
+    if(tokenA > tokenB) 
+      (tokenA, tokenB) = (tokenB, tokenA);
+    
     return PoolKey({
       token0 : tokenA,
       token1 : tokenB,
@@ -53,6 +105,7 @@ library PoolAddress {
     });
   }
 
+// Computing the pool
   function computeAddress(address deployer, PoolKey memory key) internal pure returns (address pool) {
     require(key.token0 < key.token1);
     pool = address(
--- test/FlashTest.ts
+++ test/FlashTest.ts
@@ -8,12 +8,43 @@ const WBNB = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
 const BUSD = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56';
 const CAKE = '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82';
 
-describe('Deploy getPool()', () => {
-	it('deploys and gets pool address', async () => {
-		const FlashLoan = await ethers.getContractFactory('FlashLoan');
-		let flashLoan = await FlashLoan.deploy(BUSD, WBNB, 500);
+describe('Binance FlashLoan PanCake swap V3', () => {
+	describe('Deployment and Testing', () => {
+		it('Deploys and performs Flash loan arbitrage', async () => {
+			const FlashLoan = await ethers.getContractFactory('FlashLoan');
+			let flashLoan = await FlashLoan.deploy(WBNB, BUSD, 500);
 
-		await flashLoan.deployed();
-		console.log('Flashloan contract address: \t', flashLoan.address);
+			await flashLoan.deployed();
+			console.log('Flashloan contract address: \t', flashLoan.address);
+
+			//create Signer
+			const [signer] = await ethers.getSigners();
+
+			// Initialize FlashLoan params
+			const amountBorrow = ethers.utils.parseUnits('30', 18);
+			const tokenPath = [CAKE, WBNB];
+			const exchangeRoute = [1, 0, 0];
+			const feeV3 = 500;
+
+			// Connect to the flashloan contract
+			const contractDeployedFlashload = new ethers.Contract(
+				flashLoan.address,
+				abiFlashoan,
+				signer
+			);
+
+			// Call flash Loan function
+			const txtFlashLoan = await contractDeployedFlashload.flashLoanRequest(
+				tokenPath,
+				0,
+				amountBorrow,
+				feeV3,
+				exchangeRoute
+			);
+
+			// Show Results
+			const txFlashLoanReceipt = await txtFlashLoan.wait();
+			expect(txFlashLoanReceipt.status).to.eql(1);
+		});
 	});
 });
